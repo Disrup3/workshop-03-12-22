ef31ad8860909e1c047537c7dc877c0c3a59051c
KaladinStormBlessed <davidcr.odalys@gmail.com>
2022-12-07 01:34:56 +0100

    Bet improvements

--- contracts/WorldCupBet.sol
+++ contracts/WorldCupBet.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-
 // Objetivo: Smart contract que permita a la gente adivinar el equipo que ganará el mundial.
 // Fijar sistema de lock de apuestas, no tiene mucho sentido restringirlo solo para la final.
 
@@ -9,9 +8,10 @@ pragma solidity ^0.8.9;
 contract WorldCupBet {
     address public owner;
     uint256 START_WORLDCUP_FINALMATCH = 1671379200;
+    uint256 public constant MAX_TEAMS_NUMBER = 16;
     uint256 public totalBettedAmount = 0;
     uint256 public winnerId = 100;
-    TeamInfo[16] public teamList;
+    TeamInfo[] public teamList;
     // teamId => user => amount betted
     mapping(uint256 => mapping(address => uint256)) teamUserBets;
 
@@ -19,6 +19,7 @@ contract WorldCupBet {
         uint256 id;
         string name;
         uint256 amountBetted;
+        bool defeated;
     }
 
     //------- EVENTS -------
@@ -37,7 +38,7 @@ contract WorldCupBet {
     event WorldCupBet__setWinner(uint256 teamId);
     event WorldCup__setDateTheEnd(uint256 newDate);
 
-    constructor(string[16] memory _teamList) {
+    constructor(string[] memory _teamList) {
         owner = msg.sender;
         initializeTeams(_teamList);
     }
@@ -49,38 +50,35 @@ contract WorldCupBet {
     }
 
     modifier validTeamId(uint256 teamId) {
-        // en octavos de final solo hay 16 equipos
-        require(teamId < 16, "team ID must be between 0 and 15");
+        require(
+            teamId < MAX_TEAMS_NUMBER,
+            "team ID must be between 0 and the max teams number"
+        );
         _;
     }
 
     modifier isBettingOpen() {
         require(
-            block.timestamp <= START_WORLDCUP_FINALMATCH,
+            block.timestamp <= START_WORLDCUP_FINALMATCH &&
+                winnerId > MAX_TEAMS_NUMBER,
             "Bet out of time range"
         );
         _;
     }
 
     modifier isDateTheEndEnabled(uint256 newDate) {
-        require(
-            newDate > block.timestamp,
-            "Bet out of time range"
-        );
+        require(newDate > block.timestamp, "Bet out of time range");
         _;
     }
 
-
     //------- EXTERNAL FUNCTIONS ---------
 
-    function bet(uint256 teamId)
-        external
-        payable
-        validTeamId(teamId)
-        isBettingOpen
-    {
+    function bet(
+        uint256 teamId
+    ) external payable validTeamId(teamId) isBettingOpen {
         require(msg.value > 0, "nothing to bet");
-        require(winnerId > 16);
+        require(!teamList[teamId].defeated, "The team has been defeated");
+
         teamList[teamId].amountBetted += msg.value;
         teamUserBets[teamId][msg.sender] += msg.value;
         totalBettedAmount += msg.value;
@@ -89,72 +87,77 @@ contract WorldCupBet {
 
     //check for reentrancy
     function withdraw() external {
-        require(winnerId < 16);
-        uint256 userOwedAmount = (teamUserBets[winnerId][msg.sender] *
-            totalBettedAmount) / teamList[winnerId].amountBetted;
+        require(winnerId < MAX_TEAMS_NUMBER);
+        if (teamList[winnerId].amountBetted > 0) {
+            uint256 userOwedAmount = (teamUserBets[winnerId][msg.sender] *
+                totalBettedAmount) / teamList[winnerId].amountBetted;
 
-        require(userOwedAmount > 0, "nothing to withdraw");
+            require(userOwedAmount > 0, "nothing to withdraw");
+            teamUserBets[winnerId][msg.sender] = 0;
 
-        teamUserBets[winnerId][msg.sender] = 0;
+            transferEth(msg.sender, userOwedAmount);
 
-        transferEth(userOwedAmount);
+            emit WorldCupBet__withdrawEarnings(
+                msg.sender,
+                userOwedAmount,
+                block.timestamp
+            );
+        } else {
+            transferEth(owner, totalBettedAmount);
+            emit WorldCupBet__withdrawEarnings(
+                owner,
+                totalBettedAmount,
+                block.timestamp
+            );
+        }
+    }
 
-        emit WorldCupBet__withdrawEarnings(
-            msg.sender,
-            userOwedAmount,
-            block.timestamp
-        );
+    function markDefeatedTeam(uint256 teamId) external {
+        teamList[teamId].defeated = true;
     }
 
     //------- INTERNAL -------
-    function transferEth(uint256 amount) internal {
-        (bool success, ) = msg.sender.call{value: amount}("");
+    function transferEth(address _to, uint256 amount) internal {
+        require(amount >= 0);
+        (bool success, ) = _to.call{value: amount}("");
         require(success, "something went wrong");
     }
 
-    function initializeTeams(string[16] memory _teamList) internal {
-        for (uint256 i = 0; i < _teamList.length; ) {
-            unchecked {
-                teamList[i].name = _teamList[i];
-                teamList[i].amountBetted = 0;
-                teamList[i].id = i;
-                ++i;
+    function initializeTeams(string[] memory _teamList) internal {
+        unchecked {
+            for (uint256 i = 0; i < _teamList.length; i++) {
+                TeamInfo memory team = TeamInfo(i, _teamList[i], 0, false);
+                teamList.push(team);
             }
         }
     }
 
     //------- ADMIN FUNCTIONS -----------
 
-    function setWinner(uint256 winnerTeamId)
-        external
-        validTeamId(winnerTeamId)
-        onlyOwner
-    {
+    function setWinner(
+        uint256 winnerTeamId
+    ) external validTeamId(winnerTeamId) onlyOwner {
         winnerId = winnerTeamId;
         emit WorldCupBet__setWinner(winnerTeamId);
     }
 
-     //------- EDIT FINAL DATE
-    function setDateFinish(uint256 newDate)
-        external
-        onlyOwner
-        isDateTheEndEnabled(newDate)
-    {
+    //------- EDIT FINAL DATE
+    function setDateFinish(
+        uint256 newDate
+    ) external onlyOwner isDateTheEndEnabled(newDate) {
         START_WORLDCUP_FINALMATCH = newDate;
         emit WorldCup__setDateTheEnd(newDate);
     }
+
     //------- VIEW FUNCTIONS -------
 
-    function getTeamList() public view returns (TeamInfo[16] memory) {
+    function getTeamList() public view returns (TeamInfo[] memory) {
         return teamList;
     }
 
-    function getAmountBettedToTeam(uint256 _id)
-        public
-        view
-        validTeamId(_id)
-        returns (uint256)
-    {
+    function getAmountBettedToTeam(
+        uint256 _id
+    ) public view validTeamId(_id) returns (uint256) {
         return teamList[_id].amountBetted;
     }
 }
--- test/WorldcupBet.test.ts
+++ test/WorldcupBet.test.ts
@@ -1,4 +1,3 @@
-
 import { time } from "@nomicfoundation/hardhat-network-helpers";
 import { expect, assert } from "chai";
 import { ethers } from "hardhat";
@@ -8,11 +7,21 @@ describe("WorldCupbet", function () {
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployOneWorldCupBetFixture() {
+    const TEAM_LIST: string[] = [
+      "Spain",
+      "Germany",
+      "England",
+      "Wales",
+      "Japan",
+      "Argentina",
+      "Japan",
+      "Arabia Saudi",
+      "Mexico",
+      "Poland",
+      "Italy",
+    ];
 
-    const TEAM_LIST: string[] = ["Spain", "Germany", "England", "Wales", "Japan", "Argentina", 
-    "Japan", "Arabia Saudi", "Mexico", "Poland", "Italy", "12", "13", "14", "15", "16"];
-    
-    const [owner, user1, user2] = await ethers.getSigners()
+    const [owner, user1, user2] = await ethers.getSigners();
 
     const WorldCupBet = await ethers.getContractFactory("WorldCupBet");
     const worldCupBet = await WorldCupBet.connect(owner).deploy(TEAM_LIST);
@@ -21,58 +30,117 @@ describe("WorldCupbet", function () {
   }
 
   describe("Deployment", function () {
-    it("should console log team list initialized in constructor",async () => {
-      const {worldCupBet, owner} = await deployOneWorldCupBetFixture();      
-      //console.log(await worldCupBet.getTeamList());
-    })
+    it("should console log team list initialized in constructor", async () => {
+      const { worldCupBet, owner } = await deployOneWorldCupBetFixture();
+      assert.lengthOf(await worldCupBet.getTeamList(), 11);
+    });
   });
 
-  describe("Betting logic", function() {
+  describe("Betting logic", function () {
     it("should bet team with id 1", async () => {
       const { worldCupBet } = await deployOneWorldCupBetFixture();
 
-      const betTxn = await worldCupBet.bet(0, {value: ethers.utils.parseEther("1")});      
+      const betTxn = await worldCupBet.bet(0, {
+        value: ethers.utils.parseEther("1"),
+      });
       expect(betTxn).to.emit(worldCupBet, "WorldCupBet_newBet");
       const amountBettedToId1 = await worldCupBet.getAmountBettedToTeam(0);
-      
-      assert(amountBettedToId1.toString() === "1000000000000000000", "value returned from view function !=")
-    })
-    
+
+      assert(
+        amountBettedToId1.toString() === "1000000000000000000",
+        "value returned from view function !="
+      );
+    });
+
     it("should withdraw the funds", async () => {
-      const { worldCupBet, owner, user1, user2 } = await deployOneWorldCupBetFixture();
-      await worldCupBet.bet(0, {value: ethers.utils.parseEther("1")});
-      await worldCupBet.connect(user1).bet(1, {value: ethers.utils.parseEther("1")});
-      await worldCupBet.connect(user2).bet(0, {value: ethers.utils.parseEther("1")});
-      
-      await worldCupBet.setWinner(0);      
+      const { worldCupBet, owner, user1, user2 } =
+        await deployOneWorldCupBetFixture();
+      await worldCupBet.bet(0, { value: ethers.utils.parseEther("1") });
+      await worldCupBet
+        .connect(user1)
+        .bet(1, { value: ethers.utils.parseEther("1") });
+      await worldCupBet
+        .connect(user2)
+        .bet(0, { value: ethers.utils.parseEther("1") });
+
+      await worldCupBet.setWinner(0);
       const withdrawTxn = await worldCupBet.withdraw();
-      
-      const block = await ethers.provider.getBlockNumber()
-      const timestamp = await ethers.provider.getBlock(block)
-      expect(withdrawTxn).to.emit(worldCupBet, "WorldCupBet__withdrawEarnings").withArgs(owner.address, 1.5, timestamp.timestamp)
-      
-      await expect(worldCupBet.withdraw()).to.be.revertedWith("nothing to withdraw")
-      })
-    })
+      const withdraw2Txn = await worldCupBet.connect(user2).withdraw();
 
-    describe("Reverts", () => {
-      it("should revert when trying to bet for a non valid teamId", async() => {
-        const { worldCupBet } = await deployOneWorldCupBetFixture();
-        await expect(worldCupBet.bet(18, {value: 100000000000})).to.be.revertedWith("team ID must be between 0 and 15")
-      })
+      const block = await ethers.provider.getBlockNumber();
+      const timestamp = await ethers.provider.getBlock(block);
+      expect(withdrawTxn)
+        .to.emit(worldCupBet, "WorldCupBet__withdrawEarnings")
+        .withArgs(owner.address, 1.5, timestamp.timestamp);
 
-      it("should not allow normal users to perform admin functions", async () => {
-        const { worldCupBet, user1 } = await deployOneWorldCupBetFixture();
-        
-        await expect(worldCupBet.connect(user1).setWinner(0)).to.be.revertedWith("Onlyowner: user not owner");
-      }) 
+      expect(withdraw2Txn)
+        .to.emit(worldCupBet, "WorldCupBet__withdrawEarnings")
+        .withArgs(user2.address, 1.5, timestamp.timestamp);
 
-      it("should not allow to bet after deadline", async () => {
-        const { worldCupBet, owner } = await deployOneWorldCupBetFixture();
-        // 1 sec after betting deadline
-        await time.increaseTo(1671379201);        
-        await expect(worldCupBet.bet(0, {value: ethers.utils.parseEther("1")})).to.be.revertedWith("Bet out of time range");
-      })           
-    })
+      await expect(worldCupBet.withdraw()).to.be.revertedWith(
+        "nothing to withdraw"
+      );
+    });
 
+    it("should withdraw the funds to owner when no one betted for the winner", async () => {
+      const { worldCupBet, owner, user1, user2 } =
+        await deployOneWorldCupBetFixture();
+      await worldCupBet
+        .connect(user1)
+        .bet(1, { value: ethers.utils.parseEther("1") });
+      await worldCupBet
+        .connect(user2)
+        .bet(0, { value: ethers.utils.parseEther("1") });
+
+      await worldCupBet.setWinner(3);
+      const withdrawTxn = await worldCupBet.withdraw();
+
+      const block = await ethers.provider.getBlockNumber();
+      const timestamp = await ethers.provider.getBlock(block);
+      expect(withdrawTxn)
+        .to.emit(worldCupBet, "WorldCupBet__withdrawEarnings")
+        .withArgs(owner.address, 2, timestamp.timestamp);
+
+      await expect(worldCupBet.withdraw()).to.be.revertedWith(
+        "something went wrong"
+      );
+    });
+  });
+
+  describe("Reverts", () => {
+    it("should revert when trying to bet for a non valid teamId", async () => {
+      const { worldCupBet } = await deployOneWorldCupBetFixture();
+      await expect(
+        worldCupBet.bet(18, { value: 100000000000 })
+      ).to.be.revertedWith(
+        "team ID must be between 0 and the max teams number"
+      );
+    });
+
+    it("should not allow normal users to perform admin functions", async () => {
+      const { worldCupBet, user1 } = await deployOneWorldCupBetFixture();
+
+      await expect(worldCupBet.connect(user1).setWinner(0)).to.be.revertedWith(
+        "Onlyowner: user not owner"
+      );
+    });
+
+    it("should not allow to bet to a defeated team", async () => {
+      const { worldCupBet, owner } = await deployOneWorldCupBetFixture();
+      // 1 sec after betting deadline
+      await worldCupBet.markDefeatedTeam(0);
+      await expect(
+        worldCupBet.bet(0, { value: ethers.utils.parseEther("1") })
+      ).to.be.revertedWith("The team has been defeated");
+    });
+
+    it("should not allow to bet after deadline", async () => {
+      const { worldCupBet, owner } = await deployOneWorldCupBetFixture();
+      // 1 sec after betting deadline
+      await time.increaseTo(1671379201);
+      await expect(
+        worldCupBet.bet(0, { value: ethers.utils.parseEther("1") })
+      ).to.be.revertedWith("Bet out of time range");
+    });
+  });
 });
